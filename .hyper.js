e7b502b4108e654773113dcd720f18c77f4d053c
Sergio Kopplin <sergiokopplin@gmail.com>
2019-06-24 11:28:21 -0300

    updates files

--- .hyper.js
+++ .hyper.js
@@ -1,21 +1,33 @@
 module.exports = {
     config: {
-      fontSize: 14,
-      fontFamily: "Fira Code, Ubuntu Mono, Menlo",
-      cursorBlink: true
+        fontSize: 14,
+        fontFamily: "Fira Code, Ubuntu Mono, Menlo",
+        cursorBlink: true,
+        hyperTabs: {
+            trafficButtons: false
+        },
+        showHamburgerMenu: false,
+        showWindowControls: false
     },
-  
+
     keymaps: {
-      "editor:copy": "ctrl+c",
-      "editor:paste": "ctrl+v",
-      "editor:break": "alt+c",
-      "pane:close": "ctrl+w",
-      "pane:splitVertical": "ctrl+d",
-      "pane:splitHorizontal": "ctrl+shift+d",
-      "tab:new": "ctrl+t",
-      "pane:next": "ctrl+2",
-      "pane:prev": "ctrl+1",
+        "editor:copy": "ctrl+c",
+        "editor:paste": "ctrl+v",
+        "editor:break": "alt+c",
+        "pane:close": "ctrl+w",
+        "pane:splitVertical": "ctrl+d",
+        "pane:splitHorizontal": "ctrl+shift+d",
+        "tab:new": "ctrl+t",
+        "pane:next": "ctrl+2",
+        "pane:prev": "ctrl+1",
+        "tab:next": "ctrl+alt+2",
+        "tab:prev": "ctrl+alt+1"
     },
-  
-    plugins: ["hyper-horizon-theme", "hypercwd"]
+
+    plugins: [
+        "hyper-horizon-theme",
+        "hypercwd",
+        "hyper-tabs-enhanced",
+        "hyper-hover-header"
+    ]
 };
