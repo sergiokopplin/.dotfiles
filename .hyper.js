66a24ffad76c5ce97545e4f352c607088dde379d
Sergio Kopplin <sergiokopplin@gmail.com>
2019-06-26 10:38:03 -0300

    updates files

--- .hyper.js
+++ .hyper.js
@@ -3,11 +3,6 @@ module.exports = {
         fontSize: 14,
         fontFamily: "Fira Code, Ubuntu Mono, Menlo",
         cursorBlink: true,
-        hyperTabs: {
-            trafficButtons: false
-        },
-        showHamburgerMenu: false,
-        showWindowControls: false
     },
 
     keymaps: {
@@ -18,16 +13,18 @@ module.exports = {
         "pane:splitVertical": "ctrl+d",
         "pane:splitHorizontal": "ctrl+shift+d",
         "tab:new": "ctrl+t",
-        "pane:next": "ctrl+2",
-        "pane:prev": "ctrl+1",
-        "tab:next": "ctrl+alt+2",
-        "tab:prev": "ctrl+alt+1"
+        // "pane:next": "ctrl+2",
+        // "pane:prev": "ctrl+1",
+        "tab:next": "ctrl+2",
+        // "tab:next": "ctrl+alt+2",
+        "tab:prev": "ctrl+1"
+        // "tab:prev": "ctrl+alt+1"
     },
 
     plugins: [
         "hyper-horizon-theme",
         "hypercwd",
         "hyper-tabs-enhanced",
-        "hyper-hover-header"
+        "hyperminimal",
     ]
 };
