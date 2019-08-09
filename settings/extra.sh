1364295a06b7849b21a1b5472336e8b5f8416b54
Sergio Kopplin <sergiokopplin@gmail.com>
2019-08-09 16:43:01 -0300

    updates files

---
+++ settings/extra.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo snap install android-studio --classic
+sudo snap install spotify
+sudo snap install slack --classic
+sudo snap install skype --classic
+
+# graphql
+# sudo snap install altair
