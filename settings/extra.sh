3c1220f5417bdd41d28c85b8c006c958e6004639
Sergio Kopplin <sergiokopplin@gmail.com>
2019-08-10 16:51:00 -0300

    updates files

--- settings/extra.sh
+++ settings/extra.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sudo snap install android-studio --classic
+sudo apt-get install openjdk-8-jre
 sudo snap install spotify
 sudo snap install slack --classic
 sudo snap install skype --classic
