c5674bd53e43387ed117271f06fe227d291316bd
Sergio Kopplin <sergiokopplin@gmail.com>
2019-08-10 18:44:58 -0300

    updates files

--- settings/extra.sh
+++ settings/extra.sh
@@ -4,9 +4,11 @@
 sudo snap install android-studio --classic
 sudo apt-get install openjdk-8-jre
 sudo apt-get install openjdk-8-jdk
-sudo apt-get install qemu-kvm libvirt-bin ubuntu-vm-builder bridge-utils ia32-libs-multiarch
+sudo apt-get install qemu-kvm libvirt-bin ubuntu-vm-builder bridge-utils
+sudo apt-get install gcc-multilib lib32z1 lib32stdc++6
 sudo adduser ${USER} kvm
 sudo chown ${USER} /dev/kvm
+~/Android/Sdk/tools/bin/sdkmanager --licenses
 
 sudo snap install spotify
 sudo snap install slack --classic
